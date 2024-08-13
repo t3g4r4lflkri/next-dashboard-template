3388cd99309749140b094e01d0fcd63280977676
Muh. Tegar Al Fikri <121480994+t3g4r4lflkri@users.noreply.github.com>
2024-08-13 14:23:00 +0000

    selesai chapter 2

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from "@/app/ui/home.module.css"; // Import CSS Module
 
 export default function Page() {
   return (
     <main className="flex flex-col min-h-screen p-6">
       <div className="flex items-end h-20 p-4 bg-blue-500 rounded-lg shrink-0 md:h-52">
         {/* <AcmeLogo /> */}
+        <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
       </div>
       <div className="flex flex-col gap-4 mt-4 grow md:flex-row">
         <div className="flex flex-col justify-center gap-6 px-6 py-10 rounded-lg bg-gray-50 md:w-2/5 md:px-20">
