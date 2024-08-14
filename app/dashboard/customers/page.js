6818b747f261cf18eb19db242e8d1bbce9921bb9
Muh. Tegar Al Fikri <121480994+t3g4r4lflkri@users.noreply.github.com>
2024-08-14 02:55:40 +0000

    selesai chapter 4

---
+++ app/dashboard/customers/page.js
@@ -0,0 +1,5 @@
+// app/dashboard/customers/page.js
+
+export default function Page(){
+    return <p>Customers Page</p>;
+}
---
+++ app/dashboard/page.js
@@ -0,0 +1,5 @@
+// app/dashboard/page.js
+
+export default function Page(){
+    return <p>Dashboard Page</p>;
+}
