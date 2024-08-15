a8fa393ebfb62f7d767c705c810c9f3edf48560b
Muh. Tegar Al Fikri <121480994+t3g4r4lflkri@users.noreply.github.com>
2024-08-15 06:36:10 +0000

    selesai chapter 9

--- app/ui/dashboard/latest-invoices.js
+++ app/ui/dashboard/latest-invoices.js
@@ -3,8 +3,10 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchLatestInvoices } from '@/app/lib/data';
 
-export default async function LatestInvoices({ latestInvoices }) {
+export default async function LatestInvoices() {
+  const latestInvoices = await fetchLatestInvoices();
   return (
     <div className="flex flex-col w-full md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
