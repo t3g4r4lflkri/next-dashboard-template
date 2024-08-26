4bf8b30a6083323fd8a3be5c67d90f6721fe5f94
t3g4r4lflkri <105841110722@student.unismuh.ac.id>
2024-08-26 22:14:24 +0800

    selesai Update chapter 11

--- app/dashboard/invoices/page.js
+++ app/dashboard/invoices/page.js
@@ -6,11 +6,12 @@ import { CreateInvoice } from "@/app/ui/invoices/buttons";
 import { lusitana } from "@/app/ui/fonts";
 import { Suspense } from "react";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { fetchInvoicesPages } from '@/app/lib/data';
 
 export default async function Page({ searchParams }) {
     const query = searchParams?.query || "";
     const currentPage = Number(searchParams?.page) || 1;
-
+    const totalPages = await fetchInvoicesPages(query);
     
   
     return (
@@ -26,7 +27,7 @@ export default async function Page({ searchParams }) {
           <Table query={query} currentPage={currentPage} />
         </Suspense>
         <div className="mt-5 flex w-full justify-center">
-          {/* <Pagination totalPages={totalPages} /> */}
+          <Pagination totalPages={totalPages} />
         </div>
       </div>
     );
